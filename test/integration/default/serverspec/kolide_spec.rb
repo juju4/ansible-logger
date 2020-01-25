1cd5bb57ed2e508f5a7e331dfe0d5ae7ab13862e
juju4 <juju4@users.noreply.github.com>
2020-01-25 09:06:17 -0500

    restore fleet port 8412

--- test/integration/default/serverspec/kolide_spec.rb
+++ test/integration/default/serverspec/kolide_spec.rb
@@ -20,39 +20,39 @@ describe process("fleet") do
   its(:count) { should eq 1 }
   it { should be_running }
 end
-describe port(8080) do
+describe port(8412) do
 #  it { should be_listening.with('tcp') }
   it { should be_listening.with('tcp6') }
 end
 
 describe file('/var/log/syslog'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
   its(:content) { should match /kolide\[[0-9]+\]: Using config file:/ }
-  its(:content) { should match /kolide\[[0-9]+\]: {"address":"0.0.0.0:8080","msg":"listening","transport":/ }
+  its(:content) { should match /kolide\[[0-9]+\]: {"address":"0.0.0.0:8412","msg":"listening","transport":/ }
   its(:content) { should_not match /kolide\[[0-9]+\]: .*permission denied.*/ }
 end
 describe file('/var/log/messages'), :if => os[:family] == 'redhat' && os[:release] == '7' do
   its(:content) { should match /kolide: Using config file:/ }
-  its(:content) { should match /kolide: {"address":"0.0.0.0:8080","msg":"listening","transport":/ }
+  its(:content) { should match /kolide: {"address":"0.0.0.0:8412","msg":"listening","transport":/ }
   its(:content) { should_not match /kolide: .*permission denied.*/ }
 end
 describe file('/var/log/messages'), :if => os[:family] == 'redhat' && os[:release] == '8' do
   its(:content) { should match /kolide\[[0-9]+\]: Using config file:/ }
-  its(:content) { should match /kolide\[[0-9]+\]: {"address":"0.0.0.0:8080","msg":"listening","transport":/ }
+  its(:content) { should match /kolide\[[0-9]+\]: {"address":"0.0.0.0:8412","msg":"listening","transport":/ }
   its(:content) { should_not match /kolide\[[0-9]+\]: .*permission denied.*/ }
 end
 
 # Note: probably requires fleet initial configuration...
-describe command('curl -vk https://localhost:8080/healthz') do
+describe command('curl -vk https://localhost:8412/healthz') do
   its(:stdout) { should match /^$/ }
 #  its(:stderr) { should match /HTTP\/1.1 200 OK/ }
   its(:exit_status) { should eq 0 }
 end
-describe command('curl -vk -X POST https://localhost:8080/api/v1/osquery/enroll') do
+describe command('curl -vk -X POST https://localhost:8412/api/v1/osquery/enroll') do
   its(:stdout) { should match /Unknown Error/ }
 #  its(:stderr) { should match /HTTP\/1.1 500 / }
 #  its(:exit_status) { should_not eq 0 }
 end
-describe command('curl -vk https://localhost:8080/hosts/manage') do
+describe command('curl -vk https://localhost:8412/hosts/manage') do
   its(:stdout) { should match /<title>Kolide Fleet<\/title>/ }
   its(:stderr) { should match /GET \/hosts\/manage/ }
   its(:stderr) { should match /HTTP\/.* 200/ }
