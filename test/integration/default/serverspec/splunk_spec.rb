0bc70cea4dc5bb268339f15cb5432a195e97608d
juju4 <juju4@users.noreply.github.com>
2020-01-25 08:16:25 -0500

    test/default: comment splunk service running check (systemd vs init.d), use process check

--- test/integration/default/serverspec/splunk_spec.rb
+++ test/integration/default/serverspec/splunk_spec.rb
@@ -5,7 +5,12 @@ set :backend, :exec
 
 describe service('splunk') do
   it { should be_enabled }
-  it { should be_running }
+#  it { should be_running }
+end
+
+describe process("splunkd") do
+  its(:user) { should eq "_splunk" }
+  its(:args) { should match /-p 8089 restart \[process-runner\]/ }
 end
 
 describe port(8000) do
